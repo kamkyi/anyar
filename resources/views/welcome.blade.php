4dfb7b1c42ba880bd475e3fa6126ec5bc2a96522
kamkyi <waihynhtun1994@gmail.com>
2020-06-28 21:29:40 +0630

    Update Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Anyar
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Anyar Thanapkha</a>
+                    <a href="#">Anyar Floral</a>
+                    <a href="#">Anyar Foods</a>
+                    <a href="#">Anyar Automobiles</a>
+                    <a href="#">Anyar Hotels & Tourism</a>
                 </div>
             </div>
         </div>
