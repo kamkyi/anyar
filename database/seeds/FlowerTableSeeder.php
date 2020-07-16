b93961c278898cec167341fc5fa09aeb9fd70c64
kamkyi <waihynhtun1994@gmail.com>
2020-07-16 08:46:57 +0630

    Update User Table Seeder

--- database/seeds/FlowerTableSeeder.php
+++ database/seeds/FlowerTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Models\Flower\Flower;
+
 class FlowerTableSeeder extends Seeder
 {
     /**
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -17,7 +17,7 @@ class UserTableSeeder extends Seeder
           [
             'name' => 'Wai',
             'email' => 'admin@admin.com',
-            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+            'password' => bcrypt('Waihyn@022008'),
           ]
         ];
 
