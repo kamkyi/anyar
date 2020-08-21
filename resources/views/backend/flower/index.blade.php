0e35e08e369c08371768f7881a3dac85cfed8014
kamkyi <waihynhtun1994@gmail.com>
2020-08-21 21:53:20 +0630

    Update Admin Panel

--- resources/views/backend/flower/index.blade.php
+++ resources/views/backend/flower/index.blade.php
@@ -1,5 +1,24 @@
 @extends('backend.layouts.datatable')
 @section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Flower</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">DataTables</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
@@ -7,7 +26,9 @@
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">DataTable with default features</h3>
+                <button type="button" class="btn btn-outline-success">Create</button>
+                <button type="button" class="btn btn-outline-success">Success</button>
+                <button type="button" class="btn btn-outline-success">Success</button>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
--- resources/views/backend/layouts/datatable.blade.php
+++ resources/views/backend/layouts/datatable.blade.php
@@ -146,26 +146,6 @@
 
   <!-- Main Sidebar Container -->
   @include('backend.partials.sidebar')
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>DataTables</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">DataTables</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
     <!-- Main content -->
     @yield('content')
     <!-- /.content -->
